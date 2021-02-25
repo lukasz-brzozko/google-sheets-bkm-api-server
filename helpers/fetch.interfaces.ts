b303b84d66a56976b004979cc2c120627dad1052
Lukasz Brzozko <lukasz9099@gmail.com>
2021-02-25 19:29:09 +0100

    fix: remove additional whitespaces

--- helpers/fetch.interfaces.ts
+++ helpers/fetch.interfaces.ts
@@ -1,28 +1,23 @@
 import { API_URLS } from "./constants";
-
 export interface BusStop {
     readonly name: string;
     readonly symbol: string;
 }
-
 export interface Line {
     readonly courses: LineCourses;
     readonly loid: number;
     readonly name: string;
     readonly number: number;
 }
-
 export interface LineCourses {
     readonly firstDirection: LineDirectionParams[];
     readonly secondDiretcion: LineDirectionParams[];
 }
-
 export interface LineDirectionParams {
     readonly color: string;
     readonly fromStopPoint: BusStop;
     readonly toStopPoint: BusStop;
 }
-
 export interface FetchParams {
     readonly contentType: string;
     readonly url: API_URLS;
